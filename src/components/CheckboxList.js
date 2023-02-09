bd7209c243197f434960b8543fcd2c6330b9ab39
joshcamp503 <joshcamp503@gmail.com>
2023-02-08 22:40:19 -0800

    Removed AddContact button from CheckboxList.js

--- src/components/CheckboxList.js
+++ src/components/CheckboxList.js
@@ -1,7 +1,5 @@
 // REACT
 import * as React from 'react';
-import AddContact from './modals/AddContact';
-import ProfileActions from './ProfileActions'
 // MUI
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -9,25 +7,10 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-// import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 
 const CheckboxList = () => {
-
-  // PAGINATION
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
 
   // CONTACT LIST TABLE
   const columns = [
@@ -58,42 +41,28 @@ const CheckboxList = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {contacts
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((contact) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={contact.name} >
-                    {columns.map((column) => {
-                      const value = contact[column.id];
-                      const label = { inputprops: { 'aria-label': 'Invitation checkbox'}}
-                      return (
-                        <TableCell key={column.id} align={column.align} padding={column.padding} sx={{ py: '8px' }} >
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value
-                          }
-                          {column.id === 'invite' && <Checkbox checked={false}/>}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
+            {contacts.map((contact) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={contact.name} >
+                  {columns.map((column) => {
+                    const value = contact[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align} padding={column.padding} sx={{ py: '8px' }} >
+                        {column.format && typeof value === 'number'
+                          ? column.format(value)
+                          : value
+                        }
+                        {column.id === 'invite' && <Checkbox checked={false}/>}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </TableContainer>
-      {/* <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={contacts.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      /> */}
     </Paper>
-
-    <AddContact />
     
     </>
   );
