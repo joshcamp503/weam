edb1bdd3e3492223e21d64419546309b07440527
joshcamp503 <joshcamp503@gmail.com>
2023-03-20 03:12:05 -0700

    Created GCP function sendSubRequest, creates a doc in activities collection

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
@@ -9,12 +10,14 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-exports.sendSubRequest = functions.firestore.document('/subRequests/{id}')
-  .onCreate((snap, context) => {
-    console.log(snap.data())
+exports.sendSubRequest = functions.firestore.document("/subRequests/{id}")
+    .onCreate((snap, context) => {
+      console.log(snap.data());
 
-    const collection = subRequests
-    const id = context.params.id
-    
-    
-  }) 
+      // const collection = "subRequests";
+      // const id = context.params.id;
+
+      const activities = admin.firestore().collection("activities");
+
+      return activities.add({text: "a new sub request was created"});
+    });
