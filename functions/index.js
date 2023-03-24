1bd481384b39d9d92023e7815cb8bc6a26402b68
joshcamp503 <joshcamp503@gmail.com>
2023-03-23 23:46:56 -0700

    Added nodemailer to gc functions, set up gc functions to send email

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const nodemailer = require("nodemailer");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
@@ -5,6 +6,17 @@ admin.initializeApp();
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
+// app password:
+// zvvjfagtbzolakki
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "joshcampdev@gmail.com",
+    pass: "zvvjfagtbzolakki",
+  },
+});
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
@@ -16,8 +28,22 @@ exports.sendSubRequest = functions.firestore.document("/subRequests/{id}")
 
       // const collection = "subRequests";
       // const id = context.params.id;
+      const dest = "joshcampdev@gmail.com";
 
-      const activities = admin.firestore().collection("activities");
+      const mailOptions = {
+        from: "Weam <joshcampdev@gmail.com>",
+        to: dest,
+        subject: "You've been invited to a game!",
+        html: "<p>You've been invited to a game!</>",
+      };
+      // returning result
+      return transporter.sendMail(mailOptions, (error, info, response) => {
+        if (error) {
+          return response.send(error.toString());
+        }
+        return response.send("Sent");
+      });
+      // const activities = admin.firestore().collection("activities");
 
-      return activities.add({text: "a new sub request was created"});
+      // return activities.add({text: "a new sub request was created"});
     });
