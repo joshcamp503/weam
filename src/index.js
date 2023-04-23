003d18f8c4fd9051930441453cc0490eec266f74
joshcamp503 <joshcamp503@gmail.com>
2023-04-22 17:19:11 -0700

    PWA: added service worker and maskable icons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon'
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AuthContextProvider } from './context/AuthContext';
@@ -20,3 +21,8 @@ root.render(
   {/* </React.StrictMode> */}
   </>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
