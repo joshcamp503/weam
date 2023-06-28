d3e1043e1f3af923cb71349a67e241887d33c34d
joshcamp503 <joshcamp503@gmail.com>
2023-06-28 11:53:38 -0700

    functions/index.js: added html template and logic to send confirmation email

--- functions/index.js
+++ functions/index.js
@@ -20,12 +20,11 @@ const inviteSource = fs.readFileSync("./invite.html", "utf-8");
 const inviteTemplate = Handlebars.compile(inviteSource);
 const rsvpSource = fs.readFileSync("./rsvp.html", "utf-8");
 const rsvpTemplate = Handlebars.compile(rsvpSource);
+const confirmationSource = fs.readFileSync("./confirmation.html", "utf-8");
+const confirmationTemplate = Handlebars.compile(confirmationSource);
 
 exports.sendSubRequest = functions.firestore.document("/subRequests/{id}")
     .onCreate((snap, context) => {
-      console.log(snap.data());
-      console.log(inviteSource);
-
       const inviteList = snap.data().invite;
       const eventInfo = {
         id: snap.data().id,
@@ -60,18 +59,19 @@ exports.sendSubRequest = functions.firestore.document("/subRequests/{id}")
 
 exports.sendRSVPRequest = functions.firestore.document("/subRequests/{id}")
     .onUpdate((snap, context) => {
+      if (snap.after.data().rsvp.pending === undefined) return false;
+      const rsvpInfo = {
+        id: snap.after.data().id,
+        creatorEmail: snap.after.data().creatorEmail,
+        name: snap.after.data().creatorName,
+        event: snap.after.data().event,
+        location: snap.after.data().location,
+        date: snap.after.data().date,
+        time: snap.after.data().time,
+        rsvpName: snap.after.data().rsvp.rsvpName,
+        rsvpEmail: snap.after.data().rsvp.rsvpName,
+      };
       if (snap.after.data().rsvp.pending) {
-        const rsvpInfo = {
-          id: snap.after.data().id,
-          creatorEmail: snap.after.data().creatorEmail,
-          name: snap.after.data().creatorName,
-          event: snap.after.data().event,
-          location: snap.after.data().location,
-          date: snap.after.data().date,
-          time: snap.after.data().time,
-          rsvpName: snap.after.data().rsvp.rsvpName,
-          rsvpEmail: snap.after.data().rsvp.rsvpName,
-        };
         const sendEmail = (contact, locals) => {
           const mailOptions = {
             from: "Weam <joshcampdev@gmail.com>",
@@ -88,8 +88,23 @@ exports.sendRSVPRequest = functions.firestore.document("/subRequests/{id}")
           });
         };
         sendEmail(rsvpInfo.creatorEmail, rsvpInfo);
-      } else {
-        return false;
+      } else if (!snap.after.data().rsvp.pending) {
+        const sendEmail = (contact, locals) => {
+          const mailOptions = {
+            from: "Weam <joshcampdev@gmail.com>",
+            to: contact,
+            subject: "Your RSVP has been accepted!",
+            html: confirmationTemplate(locals),
+          };
+          // returning result
+          return transporter.sendMail(mailOptions, (error, info, response) => {
+            if (error) {
+              return response.send(error.toString());
+            }
+            return response.send("Sent");
+          });
+        };
+        sendEmail(rsvpInfo.creatorEmail, rsvpInfo);
       }
     });
 
